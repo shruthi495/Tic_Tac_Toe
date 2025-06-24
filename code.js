55a35a206df2b3abd3e038b7e3e7b60b07f7066e
shruthi495 <shruthivodnala01@gmail.com>
2025-06-24 23:15:44 +0530

    Update code.js

--- code.js
+++ code.js
@@ -6,14 +6,14 @@ const boxArray = Array.from(boxes);
 let gameOver = false;
 
 const winningCombo = [
-  [0, 1, 2], // Top row
-  [3, 4, 5], // Middle row
-  [6, 7, 8], // Bottom row
-  [0, 3, 6], // Left column
-  [1, 4, 7], // Middle column
-  [2, 5, 8], // Right column
-  [0, 4, 8], // Diagonal top-left to bottom-right
-  [2, 4, 6]  // Diagonal top-right to bottom-left
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8],
+  [2, 4, 6]  
 ];
 function checkWin(){
     for(let combo of winningCombo){
